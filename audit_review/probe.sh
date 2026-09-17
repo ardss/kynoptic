a6c97e30a92f0257f7b08ae46b516676e78ec2b5
ardss <1339731209@qq.com>
2026-09-18 00:09:56 +0800

    chore: remove audit scratch dir from repo

--- audit_review/probe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DB="$1"; EP="$2"; PORT=${3:-18901}
-./kynoptic_copy.exe dashboard --db "$DB" --port $PORT >/dev/null 2>&1 &
-sleep 2
-curl -s "http://127.0.0.1:$PORT$EP"
-echo
-taskkill //IM kynoptic_copy.exe //F >/dev/null 2>&1
